ae25cb2d830e67fb31815f95e8867897019faa0b
sahilpocker <54647540+sahilpocker@users.noreply.github.com>
2023-09-02 12:36:00 +0530

    Initial commit

---
+++ lib/projects-data.ts
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: "Beginner software dev roadmap",
+    description: `I made this in collaboration with @dragonsandcodes`,
+    href: "https://dragons-and-codes.notion.site/100-days-of-becoming-a-full-stack-developer-challenge-dragonsandcodeschallenge-0dfc9f2e19784a498e0bbd05fa199f9c?ref=thedevdavid",
+    mediaSrc: "/project-notion.png",
+    mediaType: "image",
+  },
+  {
+    title: "Developer Digital Garden",
+    description: `An open source blogging (digital gardening) template for developers using Next.js app router, MDX, Contentlayer, Tailwind CSS, @shadcn/ui, Lucide Icons, and more.`,
+    href: "https://github.com/thedevdavid/digital-garden",
+    mediaSrc: "/project-garden.mp4",
+    mediaType: "video",
+  },
+];
